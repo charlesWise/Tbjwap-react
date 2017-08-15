fd220ddb1e13985e4945db72190684c39d4eb38e
lianqin <chenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.com>
2017-08-15 18:26:07 +0800

    投资tab

--- src/views/invest/InvestNavigation.js
+++ src/views/invest/InvestNavigation.js
@@ -9,7 +9,7 @@ class InvestNavigation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            curSeries: 0
         }
     }
 
@@ -17,14 +17,23 @@ class InvestNavigation extends React.Component {
 
     }
 
+    _prdSeries(type) {
+        this.props.prjSeries(type);
+        this.setState({ curSeries: type })
+    }
+
     render() {
         return (
             <section className="invest-nav-wrap">
                 <ul>
-                    <li className="active" onClick={() => { this.props.prjSeries(0) }}><a href="javascript:;">全部</a></li>
-                    <li onClick={() => { this.props.prjSeries(1) }}><a href="javascript:;">盾系</a></li>
-                    <li onClick={() => { this.props.prjSeries(2) }}><a href="javascript:;">赢系</a></li>
-                    <li onClick={() => { this.props.prjSeries(3) }}><a href="javascript:;">铂系</a></li>
+                    <li className={this.state.curSeries == 0 && 'active'}
+                        onClick={() => { this._prdSeries(0) }}><a href="javascript:;">全部</a></li>
+                    <li className={this.state.curSeries == 1 && 'active'}
+                        onClick={() => { this._prdSeries(1) }}><a href="javascript:;">盾系</a></li>
+                    <li className={this.state.curSeries == 2 && 'active'}
+                        onClick={() => { this._prdSeries(2) }}><a href="javascript:;">赢系</a></li>
+                    <li className={this.state.curSeries == 3 && 'active'}
+                        onClick={() => { this._prdSeries(3) }}><a href="javascript:;">铂系</a></li>
                 </ul>
             </section>
         )
