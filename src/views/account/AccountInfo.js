f56e310d10efdf36e5a70460d09242005e1bf0f3
lianqin <chenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.com>
2017-08-17 15:09:33 +0800

    账户

--- src/views/account/AccountInfo.js
+++ src/views/account/AccountInfo.js
@@ -14,8 +14,28 @@ class AccountInfo extends React.Component {
     render() {
         return (
             <section className="account-info-wrap">
-                <div className="account">
-                    
+                <div className="account-head">
+                    <div className="image-date">
+                        <i className="icon-user"></i>
+                        <i className="icon-msg"></i>
+                        <i className="icon-sign"></i>
+                    </div>
+                    <div className="means-money">
+                        <p className="name">净资产(元)<i className="icon-eye-open"></i></p>
+                        <p className="money">1,820,687,389.93</p>
+                    </div>
+                    <div className="profit">
+                        <ul>
+                            <li>
+                                <p>1,564,213,092.93</p>
+                                <p>可用余额(元)</p>
+                            </li>
+                            <li>
+                                <p>529.92</p>
+                                <p>预计总收益(元)</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         )
